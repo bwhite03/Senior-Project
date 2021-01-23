aa475bc497bdd410d47edefcd69b6768e94e13e8
bwhite03 <bwhite1195@yahoo.com>
2021-01-22 20:27:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+// const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+
+// dotenv.config();
+
+// Middleware
+//app.use(express.static("client/build"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect database
+// mongoose.connect(process.env.DB_CONNECT, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
